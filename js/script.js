81ce044fc247cf8355146924f19f6f87b0d80d79
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-11-20 20:59:19 +0100

    Add 2 last sections with img

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 let flag = 0;
 
 document.addEventListener('scroll', function () {
-    const map = document.querySelector('.world-map-img');
+    const map = document.querySelector('.world-map');
     if (isInViewport(map)) {
         const places = document.querySelector('.places-img');
         const world = document.querySelector('.world-img');
         world.style.display = "none";
         places.style.display = "block";
         if (flag == 0) {
+            places.src = "";
             places.src = "images/1.gif";
             flag = 1;
         }
@@ -18,7 +19,6 @@ document.addEventListener('scroll', function () {
         world.style.display = "block";
         places.style.display = "none";
         flag = 0;
-        places.src = "";
     }
 })
 
@@ -26,18 +26,14 @@ function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
         rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= ((window.innerHeight) || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.bottom <= (window.innerHeight)
     );
 }
 
 function outOfViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom >= ((window.innerHeight + 800) || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.top > window.innerHeight ||
+        rect.bottom < 0
     );
 }
